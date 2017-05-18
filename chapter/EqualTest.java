fbff5eeaa73277644c339fca241fd0a5132b953d
magicRain <1303637389@qq.com>
2017-05-18 17:00:59 +0800

    init EqualTest.json

---
+++ chapter/EqualTest.java
@@ -0,0 +1,14 @@
+package com.shlg.chuang.chapter;
+
+public class EqualTest {
+    
+    static String lab = "a";
+
+    public static void main(String[] args) {
+        String item = null;
+        if (item.equals(lab)) {
+            System.out.println("Hello World");
+        }
+    }
+
+}
